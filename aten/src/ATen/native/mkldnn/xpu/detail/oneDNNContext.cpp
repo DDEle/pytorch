634b83d4d6e64955642e528d7d80313116593771
Ding, Yi1 <yi1.ding@intel.com>
2024-12-24 01:38:53 +0000

    Revert all non-version changes

--- aten/src/ATen/native/mkldnn/xpu/detail/oneDNNContext.cpp
+++ aten/src/ATen/native/mkldnn/xpu/detail/oneDNNContext.cpp
@@ -1,8 +1,5 @@
 #include <ATen/native/mkldnn/xpu/detail/Utils.h>
 #include <ATen/native/mkldnn/xpu/detail/oneDNNContext.h>
-#include <c10/xpu/XPUCachingAllocator.h>
-#include <oneapi/dnnl/dnnl_graph.hpp>
-#include <oneapi/dnnl/dnnl_graph_sycl.hpp>
 
 /* *
  * Do NOT put any kernels or call any device binaries here!
@@ -11,45 +8,6 @@
 namespace at::native::onednn {
 
 using namespace dnnl;
-
-static inline void* dnnl_alloc(
-    size_t size,
-    size_t /*alignment*/,
-    const void* /*dev*/,
-    const void* /*context*/) {
-  return c10::xpu::XPUCachingAllocator::raw_alloc(size);
-}
-
-static inline void dnnl_delete(
-    void* buf,
-    const void* /*dev*/,
-    const void* /*context*/,
-    void* /*event*/) {
-  return c10::xpu::XPUCachingAllocator::raw_delete(buf);
-}
-
-GpuEngineManager::GpuEngineManager() {
-  c10::DeviceIndex device_count = c10::xpu::device_count();
-  TORCH_INTERNAL_ASSERT(device_count > 0);
-  for (const auto i : c10::irange(device_count)) {
-    static dnnl::graph::allocator alloc =
-        dnnl::graph::sycl_interop::make_allocator(dnnl_alloc, dnnl_delete);
-    engine_pool.push_back(std::make_shared<dnnl::engine>(
-        dnnl::graph::sycl_interop::make_engine_with_allocator(
-            c10::xpu::get_raw_device(i),
-            c10::xpu::get_device_context(),
-            alloc)));
-  }
-
-  // Set default xpu constant_tensor_cache_capacity. Ref:
-  // https://oneapi-src.github.io/oneDNN/dev_guide_constant_tensor_cache.html
-  if (!c10::utils::get_env("ONEDNN_GRAPH_CONSTANT_TENSOR_CACHE_CAPACITY")
-           .has_value()) {
-    TORCH_INTERNAL_ASSERT(
-        dnnl_success ==
-        dnnl_graph_set_constant_tensor_cache_capacity(dnnl_gpu, 1024));
-  }
-}
 
 GpuEngineManager& GpuEngineManager::Instance() {
   static GpuEngineManager myInstance;
